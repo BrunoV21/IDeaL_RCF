ae8b9e5a61d9a6d6736dc9ef2cfa925b3f6278f2
BrunoV21 <bruno.vitorino@tecnico.ulisboa.pt>
2024-06-27 01:02:54 +0100

    removed unecessary bloat

--- ideal_rcf/dataloader/config.py
+++ ideal_rcf/dataloader/config.py
@@ -1,7 +1,5 @@
 from typing import List, Optional
 
-zero_features = ['I1_7', 'I1_10', 'I1_11', 'I1_12', 'I1_13', 'I1_14', 'I1_16', 'I1_18', 'I1_20', 'I2_8', 'I2_9',
-                         'I2_10', 'I2_11', 'I2_12', 'I2_13', 'I2_14', 'I2_15', 'I2_16', 'I2_17', 'I2_18', 'I2_19', 'I2_20']
 
 class config(object):
     def __init__(self,
@@ -57,4 +55,3 @@ class config(object):
             return attribute[0]
         else:
             return attribute
-    
