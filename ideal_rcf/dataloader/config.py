ded4b2110f51cde1eaf05b085d6ab274f4bfa4f8
BrunoV21 <bruno.vitorino@tecnico.ulisboa.pt>
2024-06-29 00:27:35 +0100

    included support for features cardinality to work with features filter and transformations

--- ideal_rcf/dataloader/config.py
+++ ideal_rcf/dataloader/config.py
@@ -20,11 +20,13 @@ class config(object):
                  labels_eV :Optional[List[str]]=None,
                  labels_NL :Optional[List[str]]=None,
                  features_filter :Optional[List[str]]=None,
+                 features_cardinality :Optional[List[int]]=None,
                  features_z_score_outliers_threshold :Optional[int]=None,
                  Cx :Optional[str]='Cx',
                  Cy :Optional[str]='Cy',
                  u_velocity_label :Optional[str]='um',
                  v_velocity_label :Optional[str]='vm',
+                 debug :Optional[bool]=False,
                  dataset_labels_dir :Optional[str]='labels') -> None:
         
         self.cases = self.ensure_list_instance(cases)
@@ -53,15 +55,27 @@ class config(object):
         self.labels_eV = self.ensure_str_instance(labels_eV)
 
         self.features_filter = self.ensure_list_instance(features_filter)
+        self.features_cardinality = self.ensure_list_instance(features_cardinality)
+
+        if self.features_filter or self.features_cardinality:
+            try:
+                assert len(self.features) == len(self.features_cardinality),\
+                    f'''features_filter and features_cardinality should be passed at the same time. Features and features_cardinalities must be lists with same lenghts but got {len(self.features)} and {len(self.features_cardinality)}'''
+            
+            except TypeError:
+                raise TypeError(f'''features_filter and features_cardinality should be passed at the same time. Features and features_cardinalities must be lists with same lenghts but got {self.features} and {self.features_cardinality}''')
+            
         self.remove_outliers_threshold = features_z_score_outliers_threshold
 
         self.Cx = Cx
         self.Cy = Cy
 
-        self.u_velocity_label = u_velocity_label
-        self.v_velocity_label = v_velocity_label
+        self.u = u_velocity_label
+        self.v = v_velocity_label
 
         self.dataset_labels_dir = dataset_labels_dir
+
+        self.debug = debug
     
 
     scalers_obj = {
